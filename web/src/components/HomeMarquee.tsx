c6173bd5c9dd5a5752fb5d3b17b37379638db0e0
Keiran Moores <74584957+Keiran39493@users.noreply.github.com>
2026-06-25 21:08:16 +0200

    Delete web directory

--- web/src/components/HomeMarquee.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useLocale } from "@/context/LocaleContext";
-import { MarqueeSection } from "./MarqueeSection";
-
-export function HomeMarquee() {
-  const { tr } = useLocale();
-  return <MarqueeSection title={tr("marquee_title")} />;
-}
--- web/src/lib/ngos.ts
+++
@@ -1,7 +0,0 @@
-import ngosData from "@/data/ngos.json";
-import type { NGO } from "./types";
-
-export function getAllNgos(): NGO[] {
-  const ngos = (ngosData as { ngos: NGO[] }).ngos;
-  return [...ngos].sort((a, b) => a.name.localeCompare(b.name, "de"));
-}
